cfe29e7b8a75f3865e8f2166fa9260f27a8381bb
imissr <khaledlovmnawie@hotmail.com>
2024-10-11 18:27:42 +0200

    new

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -24,7 +24,7 @@ public class Client {
         }catch (UnknownHostException ex) {
             System.out.println("Server not found: " + ex.getMessage());
         } catch (IOException ex) {
-            System.out.println("I/O Error: " + ex.getMessage());
+            System.out.println("I/O Error12: " + ex.getMessage());
         }
     }
 
